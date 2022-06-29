101ff9425acbb2aba2288bf89c868b44b673968e
himaja-15 <himajasagarkollati@gmail.com>
2022-06-29 21:56:19 +0530

    Web World Updated

---
+++ web.js
@@ -0,0 +1,14 @@
+// var btn=document.getElementsByClassName("btn-s");
+// var slide=document.getElementById("slide");
+// btn[0].onclick=function(){
+//     slide.style.transform="translateX(0px)";
+// }
+// btn[1].onclick=function(){
+//     slide.style.transform="translateX(-800px)";
+// }
+// btn[2].onclick=function(){
+//     slide.style.transform="translateX(-1600px)";
+// }
+// btn[3].onclick=function(){
+//     slide.style.transform="translateX(-2400px)";
+// }
